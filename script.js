b5bb02d72ebdd8d10178848692d07b10cbc1d609
Abson <absonmatepeta@gmail.com>
2025-06-06 23:52:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ window.onload = function () {
 };
 
 function loadDefaultNews() {
-  const url = `https://newsapi.org/v2/everything?q=everything&pageSize=12&language=en&apiKey=${apiKey}`;
+  const url = `https://gnews.io/api/v4/search?q=scholarship&lang=en&max=10&apikey=${apiKey}`;
   fetchAndDisplay(url);
 }
 
@@ -20,7 +20,7 @@ function searchNews() {
     alert('Please enter a search term.');
     return;
   }
-  const url = `https://newsapi.org/v2/everything?q=${query}&pageSize=12&language=en&apiKey=${apiKey}`;
+  const url = `https://gnews.io/api/v4/everything?q=${query}&pageSize=12&language=en&apiKey=${apiKey}`;
   fetchAndDisplay(url, query);
 }
 
